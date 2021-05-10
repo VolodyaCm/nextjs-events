302e1fde806275142828e00391417c8020f80b73
Volodymyr Smal-Stotskiy <volodyacm@gmail.com>
2021-05-10 13:47:30 +0300

    Add events api and change ui

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,34 @@
 import Link from 'next/link';
 import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
+import EventItem from '@/components/EventItem';
 
-export default function Home() {
+export default function Home({ events }) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcomming Events</h1>
+      { events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
+      {events.length > 0 && (
+        <Link href='/events'>
+           <a className='btn-secondary'>View All Events</a>
+        </Link>
+      )}
       <Link href="/about">About</Link>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events: events.slice(0, 3) },
+    revalidate: 1,
+  }
 }
