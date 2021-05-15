1e4772d76f6f5408e7d5ef7d96857f5862329629
Volodymyr Smal-Stotskiy <volodyacm@gmail.com>
2021-05-15 20:57:46 +0300

    Add remove event functionality

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -5,12 +5,24 @@ import { API_URL } from '@/config/index';
 import { useRouter } from 'next/router';
 import { FaPencilAlt, FaTimes } from 'react-icons/fa';
 import Image from 'next/image';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function EventPage({ evt }) {
   const router = useRouter();
-  const deleteEvent = () => {
-    console.log('delete');
+  const deleteEvent = async () => {
+    if (confirm('Are you sure?')) {
+      const res = await fetch(`${API_URL}/events/${evt.id}`, {
+        method: 'DELETE'
+      });
+
+      const data = await res.json();
+      if (!res.ok) {
+        toast.error(data.message);
+      } else {
+        router.push('/events')
+      }
+    }
   }
 
   return (
@@ -30,6 +42,7 @@ export default function EventPage({ evt }) {
           {new Date(evt.date).toLocaleDateString('en-US')} at {evt.time}
         </span>
         <h1>{evt.name}</h1>
+        <ToastContainer />
         {evt.image && (
           <div className={styles.image}>
             <Image src={evt.image.formats.medium.url} width={960} height={600} />
