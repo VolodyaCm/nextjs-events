0737730547230f1e7162e00ce73f47d0aeb752b1
Volodymyr Smal-Stotskiy <volodyacm@gmail.com>
2021-05-16 16:15:54 +0300

    Add upload image functionality

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -10,6 +10,7 @@ import moment from 'moment';
 import Image from 'next/image';
 import { FaImage } from 'react-icons/fa';
 import Modal from '@/components/Modal';
+import ImageUpload from '@/components/ImageUpload';
 
 
 export default function EditEventPage({ evt }) {
@@ -56,6 +57,13 @@ export default function EditEventPage({ evt }) {
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setValues({ ...values, [name]: value });
+  }
+
+  const imageUploaded = async () => {
+    const res = await fetch(`${API_URL}/events/${evt.id}`);
+    const data = await res.json();
+    setImagePreview(data.image.formats.thumbnail.url);
+    setShowModal(false);
   }
 
   return (
@@ -154,7 +162,7 @@ export default function EditEventPage({ evt }) {
       </div>
 
       <Modal show={showModal} onClose={() => setShowModal(false)}>
-        IMAGE UPLOAD
+        <ImageUpload evtId={evt.id} imageUploaded={imageUploaded} />
       </Modal>
     </Layout>
   )
